6c0e690f776df374eb26bcc54822099194b0f4b2
Amit212 <amomnoya@gmail.com>
2024-05-26 00:05:01 +0300

    implement chat rooms apis and refactor code

--- server/models.py
+++ server/models.py
@@ -1,6 +1,13 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
 class MessageRequest(BaseModel):
     username: str
     message: str
+
+
+class ChatRoom(BaseModel):
+    name: str
+    messages: List[MessageRequest] = []
